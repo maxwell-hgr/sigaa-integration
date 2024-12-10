f52ec55b4f72b80e6c84091273a1cac39a7f54a7
maxwell-hgr <maxwell.hygor01@gmail.com>
2024-12-10 13:26:40 -0300

    Version change

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_webcourse';
-$plugin->version = 2024121001;
+$plugin->version = 2024121002;
 $plugin->requires = 2021051700; // Requires Moodle 3.11 or higher.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '1.11';
